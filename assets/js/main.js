a86143f478173c28f793982c6dbd9e840782b2c4
pratham1456 <149098394+pratham1456@users.noreply.github.com>
2025-01-24 15:05:35 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,221 @@
+
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+const img_logo = document.getElementsByClassName('logo-img')[0];
+const navLogo = document.querySelectorAll('.nav__logo')
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+    if (this.scrollY >= 80) {
+        header.classList.add('scroll-header');
+        navLink.forEach(link => link.style.color = 'black');
+        navLogo.forEach(logo => logo.style.color = 'black');
+        img_logo.setAttribute("src", "./assets/img/1-removebg-preview (1).png");
+        
+
+    } else {
+        header.classList.remove('scroll-header');
+        navLink.forEach(link => link.style.color = '');
+        navLogo.forEach(logo => logo.style.color = '');
+        img_logo.setAttribute("src", "./assets/img/1-object-white-inverted-removebg-preview (1).png");
+    }
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== QUESTIONS ACCORDION ===============*/
+const accordionItems = document.querySelectorAll('.questions__item')
+
+accordionItems.forEach((item) =>{
+    const accordionHeader = item.querySelector('.questions__header')
+
+    accordionHeader.addEventListener('click', () =>{
+        const openItem = document.querySelector('.accordion-open')    
+
+        toggleItem(item)
+
+        if(openItem && openItem!== item){
+            toggleItem(openItem)
+        }
+    })
+})
+
+const toggleItem = (item) =>{
+    const accordionContent = item.querySelector('.questions__content')
+
+    if(item.classList.contains('accordion-open')){
+        accordionContent.removeAttribute('style')
+        item.classList.remove('accordion-open')
+    }else{
+        accordionContent.style.height = accordionContent.scrollHeight + 'px'
+        item.classList.add('accordion-open')
+    }
+
+}
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== SHOW SCROLL UP ===============*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 400 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 1200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== DARK LIGHT THEME ===============*/ 
+// const themeButton = document.getElementById('theme-button')
+// const darkTheme = 'dark-theme'
+// const iconTheme = 'ri-sun-line'
+
+// // Previously selected topic (if user selected)
+// const selectedTheme = localStorage.getItem('selected-theme')
+// const selectedIcon = localStorage.getItem('selected-icon')
+
+// // We obtain the current theme that the interface has by validating the dark-theme class
+// const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+// const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+// // We validate if the user previously chose a topic
+// if (selectedTheme) {
+//   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+//   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+//   themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+// }
+
+// // Activate / deactivate the theme manually with the button
+// themeButton.addEventListener('click', () => {
+//     // Add or remove the dark / icon theme
+//     document.body.classList.toggle(darkTheme)
+//     themeButton.classList.toggle(iconTheme)
+//     // We save the theme and the current icon that the user chose
+//     localStorage.setItem('selected-theme', getCurrentTheme())
+//     localStorage.setItem('selected-icon', getCurrentIcon())
+// })
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+// const sr = ScrollReveal({
+//     origin: 'top',
+//     distance: '60px',
+//     duration: 2500,
+//     delay: 400,
+//     // reset: true
+// })
+
+// sr.reveal(`.home__data`)
+// sr.reveal(`.home__img`, {delay: 500})
+// sr.reveal(`.home__social`, {delay: 600})
+// sr.reveal(`.about__img, .contact__box`,{origin: 'left'})
+// sr.reveal(`.about__data, .contact__form`,{origin: 'right'})
+// sr.reveal(`.steps__card, .product__card, .questions__group, .footer`,{interval: 100})
+
+
+
+const sliderControls = document.querySelector(".slider-controls");
+const sliderTabs = sliderControls.querySelectorAll(".slider-tab");
+const sliderIndicator = sliderControls.querySelector(".slider-indicator");
+// Update the indicator
+const updateIndicator = (tab, index) => {
+  document.querySelector(".slider-tab.current")?.classList.remove("current");
+  tab.classList.add("current");
+  sliderIndicator.style.transform = `translateX(${tab.offsetLeft - 20}px)`;
+  sliderIndicator.style.width = `${tab.getBoundingClientRect().width}px`;
+  // Calculate the scroll position and scroll smoothly
+  const scrollLeft = sliderTabs[index].offsetLeft - sliderControls.offsetWidth / 2 + sliderTabs[index].offsetWidth / 2;
+  sliderControls.scrollTo({ left: scrollLeft, behavior: "smooth" });
+}
+// Initialize swiper instance
+const swiper = new Swiper(".slider-container", {
+  effect: "fade",
+  speed: 1300,
+  autoplay: { delay: 5000 },
+  navigation: {
+    prevEl: "#slide-prev",
+    nextEl: "#slide-next",
+  },
+  on: {
+    // Update indicator on slide change
+    slideChange: () => {
+      const currentTabIndex = [...sliderTabs].indexOf(sliderTabs[swiper.activeIndex]);
+      updateIndicator(sliderTabs[swiper.activeIndex], currentTabIndex);
+    },
+    reachEnd: () => swiper.autoplay.stop(),
+  },
+});
+// Update the slide on tab click
+sliderTabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    swiper.slideTo(index);
+    updateIndicator(tab, index);
+  });
+});
+updateIndicator(sliderTabs[0], 0);
+window.addEventListener("resize", () => updateIndicator(sliderTabs[swiper.activeIndex], 0))
+
+
+
+// logo Image
+
+const logo = document.getElementsByClassName('logo-img');
+var swiper_test = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
